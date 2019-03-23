1b0a2a3db204f283d21afdde1c068a05c55c8444
vs001 <vs001@cox.net>
2019-03-23 15:47:22 -0700

    Added routing, issues with layout CSS

--- src/app/hero-services/hero.service.ts
+++ src/app/hero-services/hero.service.ts
@@ -1,30 +1,25 @@
 import { Injectable } from '@angular/core';
+     
 import { Observable, of } from 'rxjs';
-
+ 
 import { Hero } from '../classes/hero';
 import { HEROES } from '../classes/mock-heroes';
 import { MessageService } from '../message.service';
-
-@Injectable({
-  providedIn: 'root'
-})
-
+ 
+@Injectable({ providedIn: 'root' })
 export class HeroService {
-
+ 
   constructor(private messageService: MessageService) { }
-//  delay(ms: number) {
-//    return new Promise( resolve => setTimeout(resolve, ms) );
-//  }
-  
-  
+ 
   getHeroes(): Observable<Hero[]> {
-    //this.delay(2000);
-    console.log("service: getHeroes");
+    // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-
-  
-  
+ 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
-
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+     
 import { Hero } from '../classes/hero';
 import { HeroService } from '../hero-services/hero.service';
-import { MessageService } from '../message.service';
  
 @Component({
   selector: 'app-heroes',
@@ -9,35 +9,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-    race: "Ork"
-
-  };
   heroes: Hero[];
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    console.log("onSelect");
-    this.messageService.add('Selected Hero:' + hero.name);    
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    console.log("component: getHeroes");
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-      }
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { 
-    this.getHeroes();
-    this.selectedHero = this.heroes[0];
-  }
+ 
+  constructor(private heroService: HeroService) { }
  
   ngOnInit() {
-    //this.getHeroes();
+    this.getHeroes();
   }
  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
